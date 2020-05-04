53f3c0b6ae8e38d2a4baf0f124478788466f61d4
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-05-04 21:24:57 +0300

    Add javascript functions

--- js/scripts.js
+++ js/scripts.js
@@ -15,21 +15,24 @@ function akanFunction() {
   day = parseInt(cd);
 
 
-  var century = parseInt((year) / 100+1);
-  var formula  = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) %7;
+  var century = parseInt((year - 1) / 100 + 1);
+  var formula = (((26 * (month + 1) / 10)) + ((5 * year / 4)) + ((century / 4) - 2 * century - 1) + day) % 7;
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Afua", "Afua", "Ama"];
   var dayOfMonth = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var dayIndex = Math.floor(formula);
-      dayIndex -=1
-if (year > 0 && year < 2021) {
+  dayIndex +=1;
+
+
+  if (year > 0 && year < 2021) {
     if (month > 0 && month < 13) {
       if (day > 0 && day < 32) {
+
         if (male) {
-          document.getElementById("result").innerHTML = (' You were born on a: ' + dayOfMonth[dayIndex] + ' your Akan name is ' + maleNames[dayIndex]);
+          document.getElementById("result").innerHTML = (' You were born on a: ' + dayOfMonth[dayIndex] + ' your akan name is :' + maleNames[dayIndex]);
         } else {
-          document.getElementById("result").innerHTML = (' You were born on a: ' + dayOfMonth[dayIndex] + ' your Akan name is ' + femaleNames[dayIndex]);
-       }
+          document.getElementById("result").innerHTML = (' You were born on a: ' + dayOfMonth[dayIndex]  + ' your akan name is:' + femaleNames[dayIndex]);
+        }
 
       } else {
         alert("Input the correct date e.g 30");
